ca53b999df01a2aa58347069727a57d13d6a8062
Lucas wilman da silva crispim <Lucas Wilman da Silva Crispim>
2023-06-08 20:51:49 -0300

    correct sdk

--- src/main/java/app/repository/ProductRepository.java
+++ src/main/java/app/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepository {
-    private List<Product> products;
+    private final List<Product> products;
 
     public ProductRepository() {
         products = new ArrayList<>();
