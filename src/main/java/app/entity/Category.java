eadf0a52367b92b4d88d497f403b7bc91caaf231
Lucas wilman da silva crispim <Lucas Wilman da Silva Crispim>
2023-06-08 14:33:20 -0300

    add more classes

---
+++ src/main/java/app/entity/Category.java
@@ -0,0 +1,13 @@
+package app.entity;
+
+public class Category {
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    // Getters e setters
+
+    // Outros métodos da classe Category
+}
